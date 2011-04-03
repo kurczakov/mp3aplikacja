9d524595a416dc890168b73c2a395e4bd5942425
Kamil Rzewudzki <kurczakov@gmail.com>
2011-04-03 21:23:06 +0200

    Pierwszy komentarz

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mp3aplikacja::Application.config.secret_token = '0f063e1f9166f97bab6baf25e62648e966ebd91c47c31b061c45a7515ce60a65dd1942a7eb904517e724d11b89655b082d846fade96cb0496869560e25a6544d'
